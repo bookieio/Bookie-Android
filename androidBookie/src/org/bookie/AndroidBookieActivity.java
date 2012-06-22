ed833b275cc9347e9fa22406d2cfd355a7a1d845
Derek VerLee <dlverlee@mtu.edu>
2012-06-22 16:55:57 -0400

    Add a settings activity

--- androidBookie/src/org/bookie/AndroidBookieActivity.java
+++ androidBookie/src/org/bookie/AndroidBookieActivity.java
@@ -14,55 +14,75 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class AndroidBookieActivity extends ListActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //  setContentView(R.layout.main);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
 
-       ListView lv = getListView();
-       lv.setTextFilterEnabled(true);
 
-       lv.setOnItemClickListener(new OnItemClickListener() {
-    	   public void onItemClick(AdapterView<?> parent, View view,
-    			   int position, long id) {
-    		   String url = (String) parent.getAdapter().getItem(position);
-    		   // open link in browser
-    		   Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-    		   startActivity(browserIntent);
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
 
-    	   }
-       });
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				String url = (String) parent.getAdapter().getItem(position);
+				// open link in browser
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+				startActivity(browserIntent);
 
-       SystemNewest systemNewest = SystemNewest.getSystemNewest();
-       List<BookMark> bmarks = systemNewest.getList();
-       List<String> urls = new ArrayList<String>(bmarks.size());
-       for(BookMark item : bmarks) urls.add(item.url);
+			}
+		});
 
-       ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.list_item, urls);
-       setListAdapter(arrayAdapter);
 
-       final ListActivity bmarkListActivity = this; // TODO best practice?
+		SystemNewest systemNewest = SystemNewest.getSystemNewest();
+		List<BookMark> bmarks = systemNewest.getList();
+		List<String> urls = new ArrayList<String>(bmarks.size());
+		for(BookMark item : bmarks) urls.add(item.url);
 
-       systemNewest.addObserver(new Observer() {
-		@Override
-		public void update(Observable observable, Object data) {
-			List<BookMark> bmarks = ((SystemNewest)observable).getList();
-			List<String> urls = new ArrayList<String>(bmarks.size());
-			for(BookMark item : bmarks) urls.add(item.url);
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.list_item, urls);
+		setListAdapter(arrayAdapter);
+		setContentView(R.layout.main);
+		
+		
+		setUpSettingsButton();
+		
+		final ListActivity bmarkListActivity = this; // TODO best practice?
 
-			ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(bmarkListActivity,R.layout.list_item, urls);
-			setListAdapter(arrayAdapter);
-		}
+		systemNewest.addObserver(new Observer() {
+			@Override
+			public void update(Observable observable, Object data) {
+				List<BookMark> bmarks = ((SystemNewest)observable).getList();
+				List<String> urls = new ArrayList<String>(bmarks.size());
+				for(BookMark item : bmarks) urls.add(item.url);
 
-       });
+				ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(bmarkListActivity,R.layout.list_item, urls);
+				setListAdapter(arrayAdapter);
+			}
 
-       BookieService.getService().refreshSystemNewest();
-    }
+		});
+
+		BookieService.getService().refreshSystemNewest();
+	}
+
+	private void setUpSettingsButton() {
+		Button settingsButton = (Button) findViewById(R.id.settingsButton);
+
+		settingsButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent settingsIntent = new Intent(AndroidBookieActivity.this, Settings.class);
+				AndroidBookieActivity.this.startActivity(settingsIntent);
+			}
+		});
+	}
 }
