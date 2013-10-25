bookieio/Bookie-Android

commit e3901f72fd01d4f9b0434f608fac646c4a91d939

Up the ante - support Jelly Bean plus only (for now)
Breaks return to home on app icon click (upper left)
on the detail actvitiy.

AndroidManifest.xml
@@ -4,7 +4,7 @@
     android:versionCode="2"
     android:versionName="2012-11,0">
 
-    <uses-sdk android:minSdkVersion="10" />
+    <uses-sdk android:minSdkVersion="16" />
     <uses-permission android:name="android.permission.INTERNET"/>
 
     <application

src/main/java/us/bmark/android/BookMarkDetailActivity.java
@@ -3,7 +3,6 @@ package us.bmark.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,16 +32,6 @@ public class BookMarkDetailActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_book_mark_detail, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void dealWithIntents() {

pom.xml
@@ -19,12 +19,6 @@
         </dependency>
 
         <dependency>
-            <groupId>com.google.android</groupId>
-            <artifactId>support-v4</artifactId>
-            <version>r7</version>
-        </dependency>
-
-        <dependency>
             <groupId>us.bmark</groupId>
             <artifactId>bookieClient</artifactId>
             <version>0.0.1-SNAPSHOT</version>
@@ -44,7 +38,7 @@
                 <plugin>
                     <groupId>com.jayway.maven.plugins.android.generation2</groupId>
                     <artifactId>maven-android-plugin</artifactId>
-                    <version>3.6.0</version>
+                    <version>3.7.0</version>
                     <extensions>true</extensions>
                 </plugin>
             </plugins>

commit a8de8e1b2f08e614f695bcd2b236fcb583ab9ded

Fix up button on bookmark detail activity
Fixes bug introduced in e3901f72

AndroidManifest.xml
@@ -31,7 +31,8 @@
                 <data android:mimeType="text/plain" />
             </intent-filter>
         </activity>
-        <activity android:name="BookMarkDetailActivity"></activity>
+        <activity android:name="BookMarkDetailActivity"
+            android:parentActivityName=".BookmarkListActivity"></activity>
     </application>
 
 </manifest>

src/main/java/us/bmark/android/BookMarkDetailActivity.java
@@ -24,7 +24,7 @@ public class BookMarkDetailActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_mark_detail);
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
         dealWithIntents();
     }
 